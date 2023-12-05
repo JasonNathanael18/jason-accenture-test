dfa001894ae7e6861bebee4ccbf88714d277ae45
Jason <jasonnathanael18@gmail.com>
2023-12-05 23:11:19 +0700

    Initial Commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,75 @@
+import dependencies.*
+
+
+plugins {
+    id ("com.android.application")
+    id ("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id ("dagger.hilt.android.plugin")
+}
+
+android {
+    compileSdk = AppConfig.compileSdkVersion
+    defaultConfig {
+        namespace = AppConfig.applicationId
+        applicationId = AppConfig.applicationId
+        minSdk = AppConfig.minSdkVersion
+        targetSdk = AppConfig.targetSdkVersion
+        versionCode = AppConfig.versionCode
+        versionName = AppConfig.versionName
+        vectorDrawables{
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        debug {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        debug {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures{
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = AppConfig.kotlinCompilerExtensionVersion
+    }
+
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+//    addDataModule()
+//    addDomainModule()
+//    addDiModule()
+//    addFeatureModule()
+//    addCommonModule()
+
+    addAndroidComposeDependencies()
+    addAndroLifeCycleDependencies()
+    addCoroutinesAndroidDependencies()
+    addHiltDependencies()
+    addNetworkDependencies()
+    addLeakcanaryDependencies()
+    addMoshiDependencies()
+}
---
+++ buildSrc/src/main/java/dependencies/GroupedDepHandlerExtension.kt
@@ -0,0 +1,71 @@
+package dependencies
+import core.Dependencies
+import org.gradle.api.artifacts.dsl.DependencyHandler
+
+fun DependencyHandler.addAndroidComposeDependencies(){
+    androidComposeDependencies.forEach {
+        add("implementation",it)
+    }
+}
+
+
+fun DependencyHandler.addAndroLifeCycleDependencies(){
+    androidxLifeCycleDependencies.forEach {
+        add("implementation",it)
+    }
+}
+
+
+fun DependencyHandler.addCoroutinesAndroidDependencies(){
+    coroutinesAndroidDependencies.forEach {
+        add("implementation",it)
+    }
+}
+
+fun DependencyHandler.addCoilImageLoadingDependencies(){
+    coilImageLoadingDependencies.forEach {
+        add("implementation",it)
+    }
+}
+
+fun DependencyHandler.addNetworkDependencies(configurationName:String = "implementation"){
+    networkDependencies.forEach {
+        add(configurationName,it)
+    }
+}
+
+fun DependencyHandler.addMoshiDependencies() {
+    add("implementation",Dependencies.moshi)
+    add("kapt",Dependencies.moshiCodegen)
+}
+
+fun DependencyHandler.addHiltDependencies() {
+    add("implementation",Dependencies.hiltAndroid)
+    add("implementation",Dependencies.hiltNavCompose)
+    add("kapt",Dependencies.hiltCompiler)
+}
+
+
+fun DependencyHandler.addTimberDependencies(configurationName:String = "implementation"){
+    add(configurationName,Dependencies.timber)
+}
+
+fun DependencyHandler.addRoomDependencies() {
+    add("implementation",Dependencies.room)
+    add("implementation",Dependencies.roomKtx)
+    add("implementation",Dependencies.roomPaging)
+    add("kapt",Dependencies.roomCompiler)
+}
+
+fun DependencyHandler.addLeakcanaryDependencies(){
+    add("debugImplementation",Dependencies.leakcanary)
+}
+
+fun DependencyHandler.addAndroidTestsDependencies() {
+    add("testImplementation",Dependencies.jUnit)
+    add("androidTestImplementation",Dependencies.jUnitTestUi)
+    add("androidTestImplementation",Dependencies.jUnitExt)
+    add("androidTestImplementation",Dependencies.espresso)
+    add("debugImplementation",Dependencies.composeTooling)
+    add("debugImplementation",Dependencies.composeTestManifest)
+}
